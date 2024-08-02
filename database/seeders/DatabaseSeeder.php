65d99c50062c5c551482a438cd1ebdeeb6d0a556
AJ <yogalajay@gmail.com>
2024-08-02 19:13:10 +0545

    Seeding users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,18 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Harry',
+            'email' => 'harry@example.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Tom',
+            'email' => 'tom@example.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
         ]);
     }
 }
